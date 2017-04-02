cff48cbae79eea16203c48cd373d303c3a1102be
shibamo <qinchao@hotmail.com>
2017-04-02 14:12:15 -0700

    Init
    Mobile App for OPAS 2 business documents approval etc.
    Start from the Ionic 2.0 sidemenu template.

---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TranslateService } from 'ng2-translate/ng2-translate';
+
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import { LoginPage } from '../pages/login/login';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = Page1;
+
+  pages: Array<{title: string, component: any}>;
+
+  loginPage: any = {title: 'Login to OPAS',  component:LoginPage};
+
+  translator: TranslateService;
+  currentLanguage: string;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
+  translate: TranslateService,) {
+    this.initializeApp();
+
+    // Set the default language for translation strings, and the current language.
+    translate.setDefaultLang('cn');
+    translate.use('cn');
+    this.translator = translate;
+    this.currentLanguage = 'cn';
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Page One', component: Page1 },
+      { title: 'Page Two', component: Page2 }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  toggleLanguage(){
+    this.currentLanguage = (this.currentLanguage== 'en'?  'cn' : 'en');
+    this.translator.use(this.currentLanguage);
+    
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { Http } from '@angular/http';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate/ng2-translate';
+
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import { LoginPage } from '../pages/login/login';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { User } from '../providers/user';
+import { Api } from '../providers/api';
+
+export function createTranslateLoader(http: Http) {
+  return new TranslateStaticLoader(http, './assets/i18n', '.json');
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2,
+    LoginPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    User,
+    Api,
+  ]
+})
+export class AppModule {}
